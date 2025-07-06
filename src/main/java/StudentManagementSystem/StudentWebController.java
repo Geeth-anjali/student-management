9ca7f261d7b93c5dc06ca59b7a09684af5d4ad6a
Anjali <anjalidhanapal13@gmail.com>
2025-07-06 21:39:03 +0530

    Final updates: success message, footer, homepage styling

--- src/main/java/StudentManagementSystem/StudentWebController.java
+++ src/main/java/StudentManagementSystem/StudentWebController.java
@@ -2,6 +2,7 @@ package StudentManagementSystem;
 import StudentManagementSystem.student;
 import StudentManagementSystem.studentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,12 @@ public class StudentWebController {
 
 	@Autowired
 	private studentService studentService;
-	
+
+@GetMapping("/")
+public String home() {
+	return "home";
+}
+
 @GetMapping("/students")
 public String viewStudents(Model model) {
 List<student> student = studentService.getAllStudent();
@@ -27,11 +33,20 @@ public String showAddForm(Model model) {
     return "studentForm";
  }	
 
+@PostMapping("/students/save")
+public String saveStudent(@ModelAttribute("students") student student, RedirectAttributes redirectAttributes) {
+   studentService.saveStudent(student);
+redirectAttributes.addFlashAttribute("successMessage","Student added Successfully! 👍");
+  return "redirect:/students";
+}
+
+/*
 @PostMapping("/students/save")    
 public String saveStudent(@ModelAttribute("students") student student) {
    studentService.saveStudent(student);
      return "redirect:/students";
  }
+*/
 
 @GetMapping("/students/edit/{id}") 
 public String editStudent(@PathVariable Long id, Model model) {
